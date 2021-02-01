0cbc3e9e062a24d6599d9bd4bf2cf6922d84fcd4
Mike Zamansky <zamansky@gmail.com>
2021-02-01 10:27:39 -0500

    added fi

--- classcode/recursion/basics.cpp
+++ classcode/recursion/basics.cpp
@@ -11,6 +11,13 @@
   fact(n) = {  1 if n = 0
                n * (n-1)! otherwise
 
+
+Things to look for in a recursive solution:
+1. Base case <-- when you just know the answer without recurring
+2. reduction <-- the recursive call that will eventually get to the base case
+Things to consider
+1. Try small examples by hand
+2. look for self similarity - smaller simpler problems 
 */
 
 int fact(int n){
@@ -21,7 +28,22 @@ int fact(int n){
   }
 }
 
+/*
+ *
+ 1 1 2 3 5 8 13 21 34
 
+ the next fib number is the sum of the previous two fib numbers
+
+*/
+
+int fib(int n){
+
+  if (n <2){
+    return 1;
+  } else {
+  return fib(n-1) + fib(n-2);
+  }
+}
 int main()
 {
 
@@ -29,7 +51,12 @@ int main()
 
   for (i=0; i < 10 ; i++){
     std::cout << i << "! = " << fact(i) << "\n";
+    }
+
+    for (i=0; i < 10 ; i++){
+    std::cout << fib(i) << ", ";
     
-  }
+    }
+    std::cout << "\n";
   return 0;
 }
