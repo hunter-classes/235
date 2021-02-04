e974368850b76e61d764f100b4993c1f326feae0
Mike Zamansky <zamansky@gmail.com>
2021-02-04 11:49:04 -0500

    added hanoi

---
+++ classcode/recursion/hanoi.cpp
@@ -0,0 +1,15 @@
+
+/*
+ * 
+ can only move one disc at a time
+discs must be either in transit or on a tower (never on the ground)
+you can never place a larger disc on a smaller disc
+
+*/
+
+void hanoi(int n, char src, char dst, char tmp){
+  // base case is when n = 1 - just print out the move
+  // otherwise, figure out the recursive call part
+
+  
+}
