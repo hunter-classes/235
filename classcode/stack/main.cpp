93b861c522a93153d12616d08d62c21b02e30854
Mike Zamansky <zamansky@gmail.com>
2021-03-25 10:09:10 -0400

    added stack

---
+++ classcode/stack/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "stack.h"
+int main()
+{
+  stack *s = new stack();
+
+  std::cout << s->is_empty() << "\n";
+  s->push(5);
+  s->push(10);
+  s->push(20);
+  std::cout << s->is_empty() << "\n";
+
+  int r = s->pop();
+  std::cout << s->is_empty() << " " << r << "\n";
+  r = s->pop();
+  std::cout << s->is_empty() << " " << r << "\n";
+  r = s->pop();
+  std::cout << s->is_empty() << " " << r << "\n";
+  return 0;
+}
---
+++ classcode/stack/stack.cpp
@@ -0,0 +1,19 @@
+#include "stack.h"
+
+void stack::push(int item){
+  a[topindex] = item;
+  topindex++;
+}
+
+int stack::pop(){
+  int tmp = a[topindex-1];
+  topindex--;
+  return tmp;
+}
+
+int stack::top(){
+  return a[topindex-1];
+}
+bool stack::is_empty(){
+  return topindex==0;
+}
