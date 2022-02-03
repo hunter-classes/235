98d4ccd882ab88b5b37e5972c52ffd02b424e161
Mike Zamansky <zamansky@gmail.com>
2022-02-03 10:51:23 -0500

    added tail recursion and memoization

--- classcode/recursion/basics.cpp
+++ classcode/recursion/basics.cpp
@@ -31,6 +31,8 @@ int fib(int n){
   }
 }
 
+
+
 int fib_iter(int n){
   if (n < 2){
     return 1;
@@ -49,10 +51,43 @@ int fib_iter(int n){
   return c;
 }
 
+int fib_tail(int n, int a, int b, int c){
+  if (n == 0)
+    return a;
+  else {
+    return fib_tail(n-1,b,c,b+c);
+  }
+}
+
+int fib_t(int n){
+  return fib_tail(n,1,1,2);
+}
+
+int fibtable[10000];
+int fib_dyn(int n){
+
+  if (n < 2){
+    return 1;
+  } else if (fibtable[n] != 0){
+    return fibtable[n];
+  } else {
+    int tmp = fib_dyn(n-1) + fib_dyn(n-2);
+    fibtable[n]=tmp;
+    return tmp;
+  }
+}
+
+
 int main()
-	  {
-	    for (int i = 0; i < 100; ++i) {
-	      std::cout << i << " :  " << fib(i) << "\n";
-	    }
-	    return 0;
-	  }
+{
+  int i;
+
+  for (i=0;i<10000;i++){
+    fibtable[i]=0;
+  }
+    
+  for (i = 0; i < 100; ++i) {
+    std::cout << i << " :  " << fib_dyn(i) << "\n";
+  }
+  return 0;
+}
