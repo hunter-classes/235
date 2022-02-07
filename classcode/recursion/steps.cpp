fa099bdf56380521314bdc6ea9b983dd3c2a2e6f
Mike Zamansky <zamansky@gmail.com>
2022-02-07 10:02:52 -0500

    memoized steps

--- classcode/recursion/steps.cpp
+++ classcode/recursion/steps.cpp
@@ -20,11 +20,42 @@ int ways(int steps){
 }
 
 
+int solns[100000];
+
+int ways_mem(int steps){
+
+  // base case - we have no steps left
+  if (steps == 0)
+    return 1;
+
+  if (solns[steps] != 0){
+    return solns[steps];
+  }
+  
+  // recursive reduction
+  // how many ways can we do n-1 steps, n-2 steps and n-3 steps
+  // and add them up
+  int w = 0;
+  for (int i = 3; i >= 1; i--){
+    int r = steps - i;
+    if (r >= 0)
+      w = w + ways_mem(r);
+  }
+  solns[steps] = w;
+  return w;
+  
+}
+
+
 int main()
 {
   int n;
-  for (n=1 ; n < 40 ; n++) {
-    std::cout << n << " : " << ways(n) << "\n";
+  for (n=0;n<100000;n++){
+    solns[n]=0;
+  }
+  
+  for (n=1 ; n < 50 ; n++) {
+    std::cout << n << " : " << ways_mem(n) << "\n";
   }
   return 0;
 }
