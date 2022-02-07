ac19a2503e69963d27774d6669c2ff8ae78d05c6
Mike Zamansky <zamansky@gmail.com>
2022-02-07 10:57:05 -0500

    added base case test

--- classcode/recursion/maze/maze.cpp
+++ classcode/recursion/maze/maze.cpp
@@ -22,12 +22,36 @@ void print_maze(std::string maze[],int lines){
   }
 }
 
+void solve(std::string maze[], int lines, int row, int col){
+  // check the base cases
+
+  // base case 1are we at the exit?
+  if (maze[row][col]==goal){
+    return;
+  }
+
+  if (maze[row][col]==wall){
+    return;
+  }
+
+  
+  // add me at row col to the maze
+  maze[row][col]=me;
+  print_maze(maze,lines);
+  
+  // recusively try to solve by stepping once
+    // in each of the four drections.
+
+  
+}
+
+
 int main()
 {
   std::string maze[27];
   int lines;
   lines = load_maze("maze.dat",maze);
   print_maze(maze,lines);
-  
+  solve(maze,lines,2,2);
   return 0;
 }
