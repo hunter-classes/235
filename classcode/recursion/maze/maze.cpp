6520fd0abc934c0e42a234eda9282714a5de841e
Mike Zamansky <zamansky@gmail.com>
2022-02-10 10:33:03 -0500

    Finished maze

--- classcode/recursion/maze/maze.cpp
+++ classcode/recursion/maze/maze.cpp
@@ -6,6 +6,8 @@
 char me='Z';
 char wall=' ';
 char goal='$';
+char path='#';
+char visited='.';
 
 int load_maze(std::string filename, std::string *maze){
   std::ifstream infile(filename);
@@ -17,32 +19,41 @@ int load_maze(std::string filename, std::string *maze){
 }
 
 void print_maze(std::string maze[],int lines){
+  std::cout << "[0;0H\n";
   for(int i = 0; i < lines; i++){
     std::cout << maze[i] << "\n";
   }
 }
 
-void solve(std::string maze[], int lines, int row, int col){
+void solve(std::string maze[], int lines, int row, int col, bool &solved){
   // check the base cases
 
   // base case 1are we at the exit?
   if (maze[row][col]==goal){
-    return;
+      solved = true;
+      return;
   }
 
-  if (maze[row][col]==wall){
+  if (maze[row][col]==wall ||
+      maze[row][col]==me |
+      maze[row][col]==visited){
     return;
   }
 
   
   // add me at row col to the maze
   maze[row][col]=me;
+  usleep(80000);
   print_maze(maze,lines);
   
   // recusively try to solve by stepping once
-    // in each of the four drections.
-
+  // in each of the four drections.
+  if (!solved) solve(maze,lines,row-1,col,solved);
+  if (!solved) solve(maze,lines,row+1,col,solved);
+  if (!solved) solve(maze,lines,row,col-1,solved);
+  if (!solved) solve(maze,lines,row,col+1,solved);
   
+  if (!solved) maze[row][col]=visited;
 }
 
 
@@ -51,7 +62,13 @@ int main()
   std::string maze[27];
   int lines;
   lines = load_maze("maze.dat",maze);
+
+  std::cout << "[2J;\n";
   print_maze(maze,lines);
-  solve(maze,lines,2,2);
+  bool solved = false;
+  solve(maze,lines,2,2,solved);
+  print_maze(maze,lines);
+  std::cout << "Done!\n";
   return 0;
+  
 }
