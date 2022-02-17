94051653365e798439d1b57f08649b600f899140
Mike Zamansky <zamansky@gmail.com>
2022-02-17 10:07:51 -0500

    added pointer review

---
+++ classcode/pointers/review.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main()
+{
+  int a = 11;
+  int b = 222;
+  int ar[4]= {10,20,30,40};
+
+  int *p1,*p2;
+  int i;
+
+  
+
+  
+  p1 = &a;
+  p2 = &b;
+
+  std::cout << "p1 and a: " << &a << " " << p1 << " " << *p1 << "\n"; 
+  std::cout << "p2 and b: " << &b << " " << p2 << " " << *p2 << "\n"; 
+
+  *p1 = 400;
+
+  std::cout << a << "\n";
+
+  p1 = &(ar[0]); // this is the same as saying p1 = ar
+  std::cout << *p1 << "\n";
+  std::cout << p1[1] << "\n";
+  std::cout << p1[3] << "\n";
+  std::cout << *(p1+3) << "\n";
+  return 0;
+}
