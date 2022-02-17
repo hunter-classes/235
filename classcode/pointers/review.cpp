c85fd2173f3a818ab8aa90f3d29faaada2cc396c
Mike Zamansky <zamansky@gmail.com>
2022-02-17 10:45:25 -0500

    more pointer examples

--- classcode/pointers/review.cpp
+++ classcode/pointers/review.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
+struct s {
+  int a,b;
+};
+
 int main()
 {
   int a = 11;
-  int b = 222;
   int ar[4]= {10,20,30,40};
+  int b = 222;
 
   int *p1,*p2;
   int i;
@@ -31,6 +35,46 @@ int main()
   p1++;
 
   std::cout << *p1 << " "  << p1[0] << "\n";
+
+  std::cout << p1[-1] << "\n";
+
+  std::cout << "a: " << a << " b: " << b << "\n";
+
+  // for (int i = -20 ; i <=0; i++)
+  //   ar[i] = 111111;
   
+  // std::cout << "a: " << a << " b: " << b << "\n";
+
+  // std::cout << &a << " " << &b << " " << &(ar[0]) << "\n";
+
+
+  p1 = new int;
+  *p1 = 23;
+  std::cout << &p1 << " " << p1 << " " << *p1 << "\n";
+
+  delete p1;
+  std::cout << &p1 << " " << p1 << " " << *p1 << "\n";
+
+  p2 = new int;
+  *p2 = 12345;
+
+  // note that it might reuse the same memory if available
+  std::cout << &p1 << " " << p1 << " " << *p1 << "\n";
+
+  struct s st;
+  st.a = 10;
+  st.b = 20;
+
+  struct s *sp;
+
+  sp = &st;
+
+  std::cout << (*sp).b << "\n";
+
+  (*sp).b = 30;
+
+  std::cout << st.b << "\n";
+
+
   return 0;
 }
