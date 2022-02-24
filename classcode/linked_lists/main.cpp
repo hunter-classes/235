77fb34a0bca8b148f06fc82e9cdf4443548e3c36
Mike Zamansky <zamansky@gmail.com>
2022-02-24 10:21:38 -0500

    update simple example

--- classcode/linked_lists/main.cpp
+++ classcode/linked_lists/main.cpp
@@ -12,6 +12,24 @@ int main()
 
   std::cout << n1->getData() << "\n";
   std::cout << n2->getData() << "\n";
+
+  n1->setNext(n2);
+  std::cout << n1->getNext() << "\n";
+  std::cout << n2 << "\n";
+  std::cout << n1->getNext()->getData() << "\n";
+
+  n2->setNext(new Node("!"));
+
+  std::cout << n1->getNext()->getNext()->getData() << "\n";
+
+  Node *walker;
+  walker = n1;
+  while (walker != nullptr){
+    std::cout << walker->getData() << " ";
+    walker = walker->getNext();
+  }
+  std::cout << "\n\n";
   return 0;
 }
+
 
