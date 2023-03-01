bebbcc520471527df847ec6b072dc381d22fff6b
Mike Zamansky <zamansky@gmail.com>
2023-03-01 09:34:03 -0500

    added pointer-maze code

---
+++ code/pointer-maze/Labyrinth.cpp
@@ -0,0 +1,8 @@
+#include "Labyrinth.h"
+
+bool isPathToFreedom(MazeCell* start, const std::string& moves) {
+    /* TODO: Delete this comment and the remaining lines, then implement this function. */
+    (void) start;
+    (void) moves;
+    return false;
+}
