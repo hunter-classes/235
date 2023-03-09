60998865cdae25f7a2b3eca3e4819d352186f505
Mike Zamansky <zamansky@gmail.com>
2023-03-09 09:39:29 -0500

    added merge sort subgoals

--- code/sorting/sorts.cpp
+++ code/sorting/sorts.cpp
@@ -44,6 +44,56 @@ std::vector<int> ssort(std::vector<int> a){
   return a;
 }
 
+/*
+  left and right are already sorted low to high
+
+  create a new std::vector named merged
+  and fill it with all the elements of left and right.
+  When done, merged should be in order low to high.
+
+*/
+std::vector<int> merge(std::vector<int> left,
+		       std::vector<int> right){
+  std::vector<int> merged;
+  int l,r; // indices into left and right;
+
+  while (l < left.size() && r< right.size()){
+    if (left[l] < right[r]){
+      merged.push_back(left[l]);
+      l++;
+    } else {
+      merged.push_back(right[r]);
+      r++;
+    }
+  }
+
+  while (l < left.size()){
+    merged.push_back(left[l]);
+    l++;
+  }
+  while (r < right.size()){
+    merged.push_back(right[r]);
+    r++;
+  }
+     
+  return merged;
+}
+
+std::vector<int> msort(std::vector<int> data){
+  std::vector<int> result;)
+  // if the size of data is <= 1 just return data
+
+  // othewise
+  // split data into 2 std::vectors left and right
+  // of equal size (or off by 1)
+  // then:
+  //   left = msort(left);
+  //   right = msort(right);
+  //   result= merge(left,right);
+  // 
+  return result;
+}
+
 
 int main()
 {
@@ -56,9 +106,21 @@ int main()
   }
 
 
-  print_vector(a);
-  std::vector<int> b = ssort(a);
-  print_vector(b);
+  // print_vector(a);
+  // std::vector<int> b = ssort(a);
+  // print_vector(b);
 
+  std::vector<int> left,right;
+  for (int i = 0; i < 10; i++){
+    left.push_back(rand()%max_val);
+    right.push_back(rand()%max_val);
+  }
+  left = ssort(left);
+  right = ssort(right);
+  print_vector(left);
+  print_vector(right);
+  std::cout << "\n----------\n";
+  std::vector<int> merged = merge(left,right);
+  print_vector(merged);
   return 0;
 }
