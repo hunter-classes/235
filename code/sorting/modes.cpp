94a06812ffa663cd545f09676c4c34d51b986616
Mike Zamansky <zamansky@gmail.com>
2023-03-13 09:48:49 -0400

    wrote modes

--- code/sorting/modes.cpp
+++ code/sorting/modes.cpp
@@ -17,7 +17,9 @@ int freq(std::vector<int> v, int val){
   
 }
 
-
+// the same idiom
+// as finding the largest (or smallest)
+// in a vector/array/list
 int mode(std::vector<int> v){
   int mod_i = 0;
   int mode_so_far = v[0];
@@ -34,6 +36,38 @@ int mode(std::vector<int> v){
   return mode_so_far;
 }
 
+int fast_mode(std::vector<int> v){
+  // find the largest value
+  // takes one time through the data
+  int max_val = 0;
+  for (auto i: v){
+    if (i > max_val){
+      max_val = i;
+    }
+  }
+
+  // make a vector of size max_val to be
+  // buckets for the tallies
+  std::vector<int> counts(max_val +1);
+
+  // make the tallies
+  // takes one time through the data
+  for (auto i : v){
+    counts[i]++;
+  }
+
+  // find the largest tally
+  // takes one time through the counts vector
+  int mode_index = 0;
+  for (int i = 0; i < max_val +1; i++){
+    if (counts[i] > counts[mode_index]){
+      mode_index = i;
+    }
+  }
+  return mode_index;
+  
+}
+
 
 void print_vector(std::vector<int> a){
   for (auto i : a) {
@@ -48,7 +82,7 @@ void print_vector(std::vector<int> a){
 int main(int argc, char *argv[])
 {
   int m;
-  int size = 100; // default vector size
+  int size = 10000000; // default vector size
   int max_val = 100; // default max value for entries
 
   srand(time(NULL));
@@ -57,11 +91,12 @@ int main(int argc, char *argv[])
   for(i=0;i<size;i++){
     a[i] = rand()%max_val;    
   }
-  // print_vector(a); 
+  //print_vector(a); 
 
-  m = mode(a);
-  std::cout << "mode: " << m << "\n";
-  
+  //m = mode(a);
+  //std::cout << "mode: " << m << "\n";
+  int m2 = fast_mode(a);
+  std::cout << "mode: " << m2 << "\n";
   
   return 0;
 }
